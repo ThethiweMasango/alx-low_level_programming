33535d11ecdb5a41aa0fcdaaed5e195d67ff72e2
Thethiwe Masango <pearlmasango12@gmail.com>
2023-08-21 02:35:58 -0700

    three

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - gives the legth of char in a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
+}
+
